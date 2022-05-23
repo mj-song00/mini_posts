c9f14cd9e8382de3d089c5c64c83b64e5a15e87e
MinJi <niveus128@gmail.com>
2022-05-23 22:25:37 +0900

    구현 완료

---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../schemas/post");
+
+
+//글 저장
+router.post ('/posts', async (req, res) => {
+       const { postsId, password, title, date, contents } = req.body;
+       try{
+           const posts = await Post.find({ title });
+           if (posts.length) {
+               return res.status(400).json({ success: false, errorMessage: "제목 도배 못하지롱" })
+           }
+           const createdPosts = await Post.create({ postsId, password, title, date, contents })
+            res.json ({ posts : createdPosts });
+
+        }catch(error){
+            console.log(error)
+        }
+})
+
+//전체 글 불러오기
+router.get('/post', async (req, res) => {
+    const post = await Post.find()
+    res.json({ post })
+
+})
+
+//상세 글 불러오기
+router.get('/post/:_id', async (req, res) => {
+    const {_id } = req.params
+    const posts = await Post.findOne({_id })
+    res.json({ posts })
+})
+
+//수정 으아아아아 제발 수정되지마 ㅠㅠ  비밀번호 안넣었다고 ㅜㅜ 어 막혔다 !! 근데 비밀번호 어떻게 넣지 ㅠㅠ 어 끝났다!!
+router.put('/post/:_id', async (req, res) => {
+    const {_id} = req.params
+    const { postsId, password, title, contents } = req.body
+
+    const existId = await Post.findOne({ _id : _id})
+    if (existId.password === password ){
+        await Post.updateOne({_id: postsId} , { $set : { title, contents, }})
+        res.json({ success: true })
+    }else {
+        return res.status(400).json({ success: false, errorMessage: "비밀번호 틀렸지롱" })
+    }
+})
+
+//삭제
+router.delete('/post/:_id', async (req, res) => {
+    const {_id} = req.params
+    
+
+    const existId = await Post.find({ _id : _id})
+    if (existId.length > 0 ){
+        await Post.deleteOne({_id })
+    }
+
+    res.json({ result: "success"})
+
+})
+
+module.exports = router;
---
+++ schemas/post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const postsSchema = new mongoose.Schema({
+    postsId : {
+        type : String,
+        required : true,
+    },
+    password : {
+        type : String, 
+        required: true,
+    },
+    title : {
+        type: String,
+        required : true,
+    },
+    date : {
+        type: Date,
+        required : true,
+    },
+    contents: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model("Posts", postsSchema)
