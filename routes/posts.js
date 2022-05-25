3be46a20a94fbc302b4f10a273dc991fe9522324
MinJi <niveus128@gmail.com>
2022-05-25 17:19:44 +0900

    파일수정

--- routes/posts.js
+++ routes/posts.js
@@ -35,13 +35,13 @@ router.get('/post/:_id', async (req, res) => {
 })
 
 //수정 으아아아아 제발 수정되지마 ㅠㅠ  비밀번호 안넣었다고 ㅜㅜ 어 막혔다 !! 근데 비밀번호 어떻게 넣지 ㅠㅠ 어 끝났다!!
-router.put('/post/:_id', async (req, res) => {
+router.patch('/post/:_id', async (req, res) => {
     const {_id} = req.params
     const { postsId, password, title, contents } = req.body
 
     const existId = await Post.findOne({ _id : _id})
     if (existId.password === password ){
-        await Post.updateOne({_id: postsId} , { $set : { title, contents, }})
+        await Post.updateOne({_id: _id} , { $set : { title, contents, postsId}})
         res.json({ success: true })
     }else {
         return res.status(400).json({ success: false, errorMessage: "비밀번호 틀렸지롱" })
